fa5c058c19888068e17df0c9aced27d32b7bd122
abidhossain2 <tanvirabid229@gmail.com>
2022-11-06 21:47:35 +0600

    get random user api create

--- index.js
+++ index.js
@@ -7,18 +7,23 @@ app.use(express.json())
 
 
 
-app.get('/user/all',(req, res) => {
+// Get all user & get user with query
+
+app.get('/user/all', (req, res) => {
     fs.readFile('data.json', (err, data) => {
         if (err) {
             res.end("Problem loading data");
         } else {
             const { limit } = req.query;
             const parsedData = JSON.parse(data);
-            res.json(parsedData.slice(0,limit));
+            res.json(parsedData.slice(0, limit));
         }
     })
 })
 
+
+
+// Get a random user
 
 app.get('/user/random', (req, res) => {
 
@@ -30,13 +35,29 @@ app.get('/user/random', (req, res) => {
             const randomData = parsedData[Math.floor(Math.random() * parsedData.length)];
             res.json(randomData);
         }
-        
+
+    })
+})
+
+
+
+app.post('/user/save', (req, res) => {
+    fs.readFile('data.json', function (err, data) {
+        if (err) {
+            res.end("problem")
+        } else {
+            const json = JSON.parse(data.toString())
+            const newData = req.body;
+            json.push(newData)
+            fs.writeFileSync("data.json", JSON.stringify(json))
+            res.end("saved")
+        }
     })
 })
 
 
 app.get('/', (req, res) => {
-    res.send("Server running");
+    res.send("Server running successfully");
 })
 
 app.listen(port, () => {
